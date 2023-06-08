57f23ac816f5050e94325f3f01691c570579f9f7
Jade Freel <jade@ioxconsulting.co.za>
2023-06-08 19:53:47 +0200

    calculating the aspect ratio from width

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import time
+from PIL import Image
+
+source_folder = "./images"
+destination_folder = "./resized_images"
+
+def resize_image(image_path, width):
+    image = Image.open(image_path)
+    original_width, original_height = image.size
+
+    aspect_ratio = original_height / original_width
+
+    resized_image = image.resize((width, int(width * aspect_ratio)), Image.ANTIALIAS)
+    return resized_image
+
+def watch_folder():
+    desired_width = int(input("Enter the desired width in pixels: "))
+
+    files = os.listdir(source_folder)
+    new_files = [f for f in files if not f.startswith('.')]
+
+    for file in new_files:
+        file_path = os.path.join(source_folder, file)
+
+        resized_image = resize_image(file_path, desired_width)
+
+        destination_path = os.path.join(destination_folder, file)
+        resized_image.save(destination_path)
+
+        # os.remove(file_path)
+
+
+if __name__ == '__main__':
+    watch_folder()
